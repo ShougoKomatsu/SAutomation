78c0629e5d1571ef4908e2fbe2f3c54703be19b7
Shogo Komatsu <49989073+ShougoKomatsu@users.noreply.github.com>
2023-06-12 23:45:05 +0900

    リポジトリ移動

---
+++ SAutomation/Common.h
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+
+BOOL GetFileName(CString sFilePath, CString* sFileName);
+
+
+BOOL ReadUTFFile(CString sFilePath, CString* sData);
+BOOL ReadTextFile(CString sFilePath, CStringArray* saCommands);
