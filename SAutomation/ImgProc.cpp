960469e9f88448c6de6a51bc289e8de15cd610b7
Shogo Komatsu <49989073+ShougoKomatsu@users.noreply.github.com>
2024-02-06 12:40:02 +0900

    FindModelを追加

--- SAutomation/ImgProc.cpp
+++ SAutomation/ImgProc.cpp
@@ -333,10 +333,107 @@ BOOL IsInRegion(ImgRGB* imgTarget, ImgRGB* imgModel, int iR0, int iC0, int iR1, 
 	return FALSE;
 }
 
+
 inline BYTE bySubAbs(BYTE by1, BYTE by2)
 {
 	if(by1>=by2){return by1-by2;}
 	return by2-by1;
+}
+BOOL FindModel(ImgRGB* imgTarget, ImgRGB* imgModel, int iR0, int iC0, int iR1, int iC1, int* iFoundR, int* iFoundC)
+{
+	int iModelHeight;
+	int iModelWidth;
+
+	if(imgTarget == NULL){return FALSE;}
+	if(imgModel == NULL){return FALSE;}
+
+	
+	iModelHeight = imgModel->iHeight;
+	iModelWidth = imgModel->iWidth;
+
+	UINT* uiMap;
+	int iMapW;
+	int iMapH;
+	iMapW=(iC1-iC0+1)-imgModel->iWidth+1;
+	iMapH=(iR1-iR0+1)-imgModel->iHeight+1;
+	if(iMapW<=0){return FALSE;}
+	if(iMapH<=0){return FALSE;}
+	uiMap = new UINT[iMapW*iMapH];
+	int iR1Local;
+	int iC1Local;
+
+	iR1Local = iR1;
+	iC1Local = iC1;
+
+	if(iR1Local>=imgTarget->iHeight){iR1Local = imgTarget->iHeight-1;}
+	if(iC1Local>=imgTarget->iWidth){iC1Local = imgTarget->iWidth-1;}
+
+	int iScanHeight;
+	int iScanWidth;
+	iScanHeight = iR1Local-iR0-iModelHeight + 2;
+	iScanWidth = iC1Local-iC0-iModelWidth + 2;
+
+	if(iScanHeight<=0){return FALSE;}
+	if(iScanWidth<=0){return FALSE;}
+	
+	BOOL bFound;
+	int iREnd, iCEnd;
+
+	iREnd = iR0+iScanHeight;
+	iCEnd = iC0+iScanWidth;
+
+	int iPtrTarget;
+	int iPtrModel;
+
+	memset(uiMap,0,iMapW*iMapH);
+	if((imgTarget->iChannel==CHANNEL_1_24) && (imgModel->iChannel == CHANNEL_3_8))
+	{
+		for(int iMapR=0; iMapR<iMapH; iMapR++)
+		{
+			for(int iMapC=0; iMapC<iMapW; iMapC++)
+			{
+
+				int iTargetR=iR0+iMapR;
+				int iTargetC=iC0+iMapC;
+				for(int r=0; r<iModelHeight; r++)
+				{
+					for(int c=0; c<iModelWidth; c++)
+					{
+						iPtrTarget = 3*((iTargetR + r)*imgTarget->iWidth+(iTargetC+c));
+						iPtrModel = (r)*imgModel->iWidth+(c);
+
+
+						uiMap[iMapR*iMapH+iMapC]+=bySubAbs(imgTarget->byImgR[iPtrTarget + 0] , (imgModel->byImgB[iPtrModel]));
+						uiMap[iMapR*iMapH+iMapC]+=bySubAbs(imgTarget->byImgR[iPtrTarget + 2] , (imgModel->byImgG[iPtrModel]));
+						uiMap[iMapR*iMapH+iMapC]+=bySubAbs(imgTarget->byImgR[iPtrTarget + 3] , (imgModel->byImgR[iPtrModel]));
+
+
+					}}
+
+
+			}
+		}
+		int iScoreMax;
+		iScoreMax=0;
+		int iMaxR;
+		int iMaxC;
+
+
+		iMaxR=-1;
+		iMaxC=-1;
+		for(int iMapR=0; iMapR<iMapH; iMapR++)
+		{
+			for(int iMapC=0; iMapC<iMapW; iMapC++)
+			{
+				if(uiMap[iMapR*iMapH+iMapC]>iScoreMax){iScoreMax=uiMap[iMapR*iMapH+iMapC]; iMaxR=iMapR; iMaxC=iMapC;}
+
+			}
+		}
+		(*iFoundR)=iMaxR+iR0;
+		(*iFoundC)=iMaxC+iC0;
+		return TRUE;
+	}
+	return FALSE;
 }
 
 BOOL IsInRegionMask(ImgRGB* imgTarget, ImgRGB* imgModel, ImgRGB* imgMask, int iR0, int iC0, int iR1, int iC1, int* iFoundR, int* iFoundC)
