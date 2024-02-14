797184d9e372d25b166a5c4f47e5bc481b823c5f
Shogo Komatsu <49989073+ShougoKomatsu@users.noreply.github.com>
2024-02-14 12:45:48 +0900

    ピラミッドモデル版サーチ関数準備

--- SAutomation/ImgProc.cpp
+++ SAutomation/ImgProc.cpp
@@ -339,6 +339,187 @@ inline BYTE bySubAbs(BYTE by1, BYTE by2)
 	if(by1>=by2){return by1-by2;}
 	return by2-by1;
 }
+
+BOOL CropImage2(ImgRGB* imgRGBin, ImgRGB* imgRGBout, int iR0, int iC0, int iR1, int iC1)
+{
+	if(imgRGBin==NULL){return FALSE;}
+	if(imgRGBout==NULL){return FALSE;}
+	if(iR0>iR1){return FALSE;}
+	if(iC0>iC1){return FALSE;}
+	if(iR0<0){return FALSE;}
+	if(iC0<0){return FALSE;}
+	if(iR1>=imgRGBin->iHeight){return FALSE;}
+	if(iC1>=imgRGBin->iWidth){return FALSE;}
+
+	if(imgRGBin==imgRGBout)
+	{
+		//
+	}
+	else
+	{
+		imgRGBout->Init();
+		int iWidthP1;
+		int iHeightP1;
+		if(((iC1-iC0+1)%2)==0){iWidthP1=0;}else{iWidthP1=1;}
+		if(((iR1-iR0+1)%2)==0){iHeightP1=0;}else{iHeightP1=1;}
+
+		imgRGBout->Set(iC1-iC0+1+iWidthP1, iR1-iR0+1+iHeightP1, imgRGBin->iChannel);
+		if(imgRGBin->iChannel==CHANNEL_1_24)
+		{
+
+			for(int r=iR0; r<=iR1; r++)
+			{
+				for(int c=iC0; c<=iC1; c++)
+				{
+					imgRGBout->byImgR[3*((r-iR0)*imgRGBout->iWidth+(c-iC0))+0]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+0];
+					imgRGBout->byImgR[3*((r-iR0)*imgRGBout->iWidth+(c-iC0))+1]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+1];
+					imgRGBout->byImgR[3*((r-iR0)*imgRGBout->iWidth+(c-iC0))+2]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+2];
+				}
+				if(iWidthP1==1)
+				{
+					int c=iC1;
+					imgRGBout->byImgR[3*((r-iR0)*imgRGBout->iWidth+(c-iC0+1))+0]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+0];
+					imgRGBout->byImgR[3*((r-iR0)*imgRGBout->iWidth+(c-iC0+1))+1]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+1];
+					imgRGBout->byImgR[3*((r-iR0)*imgRGBout->iWidth+(c-iC0+1))+2]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+2];
+				}
+			}
+			if(iHeightP1==1)
+			{
+				int r=iR1;
+				for(int c=iC0; c<=iC1; c++)
+				{
+					imgRGBout->byImgR[3*((r+1-iR0)*imgRGBout->iWidth+(c-iC0))+0]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+0];
+					imgRGBout->byImgR[3*((r+1-iR0)*imgRGBout->iWidth+(c-iC0))+1]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+1];
+					imgRGBout->byImgR[3*((r+1-iR0)*imgRGBout->iWidth+(c-iC0))+2]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+2];
+				}
+				if(iWidthP1==1)
+				{
+					int c=iC1;
+					imgRGBout->byImgR[3*((r+1-iR0)*imgRGBout->iWidth+(c-iC0+1))+0]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+0];
+					imgRGBout->byImgR[3*((r+1-iR0)*imgRGBout->iWidth+(c-iC0+1))+1]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+1];
+					imgRGBout->byImgR[3*((r+1-iR0)*imgRGBout->iWidth+(c-iC0+1))+2]=imgRGBin->byImgR[3*(r*imgRGBin->iWidth+c)+2];
+				}
+			}
+		}
+		if(imgRGBin->iChannel==CHANNEL_3_8)
+		{
+			for(int r=iR0; r<=iR1; r++)
+			{
+				for(int c=iC0; c<=iC1; c++)
+				{
+					imgRGBout->byImgB[(r-iR0)*imgRGBout->iWidth+(c-iC0)]=imgRGBin->byImgB[r*imgRGBin->iWidth+c];
+					imgRGBout->byImgG[(r-iR0)*imgRGBout->iWidth+(c-iC0)]=imgRGBin->byImgG[r*imgRGBin->iWidth+c];
+					imgRGBout->byImgR[(r-iR0)*imgRGBout->iWidth+(c-iC0)]=imgRGBin->byImgR[r*imgRGBin->iWidth+c];
+
+				}
+				if(iWidthP1==1)
+				{
+					int c=iC1;
+					imgRGBout->byImgB[(r-iR0)*imgRGBout->iWidth+(c-iC0+1)]=imgRGBin->byImgB[r*imgRGBin->iWidth+c];
+					imgRGBout->byImgG[(r-iR0)*imgRGBout->iWidth+(c-iC0+1)]=imgRGBin->byImgG[r*imgRGBin->iWidth+c];
+					imgRGBout->byImgR[(r-iR0)*imgRGBout->iWidth+(c-iC0+1)]=imgRGBin->byImgR[r*imgRGBin->iWidth+c];
+				}
+			}
+			if(iHeightP1==1)
+			{
+				int r=iR1;
+				for(int c=iC0; c<=iC1; c++)
+				{
+					imgRGBout->byImgB[(r+1-iR0)*imgRGBout->iWidth+(c-iC0)]=imgRGBin->byImgB[r*imgRGBin->iWidth+c];
+					imgRGBout->byImgG[(r+1-iR0)*imgRGBout->iWidth+(c-iC0)]=imgRGBin->byImgG[r*imgRGBin->iWidth+c];
+					imgRGBout->byImgR[(r+1-iR0)*imgRGBout->iWidth+(c-iC0)]=imgRGBin->byImgR[r*imgRGBin->iWidth+c];
+
+				}
+				if(iWidthP1==1)
+				{
+					int c=iC1;
+					imgRGBout->byImgB[(r+1-iR0)*imgRGBout->iWidth+(c-iC0+1)]=imgRGBin->byImgB[r*imgRGBin->iWidth+c];
+					imgRGBout->byImgG[(r+1-iR0)*imgRGBout->iWidth+(c-iC0+1)]=imgRGBin->byImgG[r*imgRGBin->iWidth+c];
+					imgRGBout->byImgR[(r+1-iR0)*imgRGBout->iWidth+(c-iC0+1)]=imgRGBin->byImgR[r*imgRGBin->iWidth+c];
+				}
+			}
+		}
+		if(imgRGBin->iChannel==CHANNEL_1_8)
+		{
+			for(int r=iR0; r<=iR1; r++)
+			{
+				for(int c=iC0; c<=iC1; c++)
+				{
+					imgRGBout->byImgR[(r-iR0)*imgRGBout->iWidth+(c-iC0)]=imgRGBin->byImgR[r*imgRGBin->iWidth+c];
+
+				}
+				if(iWidthP1==1)
+				{
+					int c=iC1;
+					imgRGBout->byImgR[(r-iR0)*imgRGBout->iWidth+(c-iC0+1)]=imgRGBin->byImgR[r*imgRGBin->iWidth+c];
+				}
+			}
+			if(iHeightP1==1)
+			{
+				int r=iR1;
+				for(int c=iC0; c<=iC1; c++)
+				{
+					imgRGBout->byImgR[(r+1-iR0)*imgRGBout->iWidth+(c-iC0)]=imgRGBin->byImgR[r*imgRGBin->iWidth+c];
+				}	
+				if(iWidthP1==1)
+				{
+					int c=iC1;
+					imgRGBout->byImgR[(r+1-iR0)*imgRGBout->iWidth+(c-iC0+1)]=imgRGBin->byImgR[r*imgRGBin->iWidth+c];
+				}
+			}
+		}
+
+
+	}
+	return TRUE;
+}
+
+BOOL CreatePyramid(ImgRGB* imgIn, ImgRGB* imgOut)
+{
+	if(imgIn == NULL){return FALSE;}
+	if(imgOut == NULL){return FALSE;}
+	if(imgIn == imgOut){return FALSE;}
+
+	int iWidthOriginal=imgIn->iWidth;
+	int iHeightOriginal=imgIn->iHeight;
+	int iWidthNew;
+	int iHeightNew;
+	if((iWidthOriginal%2)!=0){return FALSE;}
+	if((iHeightOriginal%2)!=0){return FALSE;}
+
+	iHeightNew=iHeightOriginal>>1;
+	iWidthNew=iWidthOriginal>>1;
+
+	imgOut->Set(iWidthNew, iHeightNew, imgIn->iChannel);
+	UINT uiData;
+	if(imgIn->iChannel==CHANNEL_1_24)
+	{
+		for(int r=0; r<iHeightNew; r++)
+		{for(int c=0; c<iWidthNew; c++)
+		{
+			uiData = imgIn->byImgR[3*(((2*r+0)*iWidthOriginal)+(2*c+0))+0]
+			+imgIn->byImgR[3*(((2*r+0)*iWidthOriginal)+(2*c+1))+0]
+			+imgIn->byImgR[3*(((2*r+1)*iWidthOriginal)+(2*c+0))+0]
+			+imgIn->byImgR[3*(((2*r+1)*iWidthOriginal)+(2*c+1))+0];
+			imgOut->byImgR[3*(r*iWidthNew+c)+0]=uiData>>2;
+
+			uiData = imgIn->byImgR[3*(((2*r+0)*iWidthOriginal)+(2*c+0))+1]
+			+imgIn->byImgR[3*(((2*r+0)*iWidthOriginal)+(2*c+1))+1]
+			+imgIn->byImgR[3*(((2*r+1)*iWidthOriginal)+(2*c+0))+1]
+			+imgIn->byImgR[3*(((2*r+1)*iWidthOriginal)+(2*c+1))+1];
+			imgOut->byImgR[3*(r*iWidthNew+c)+1]=uiData>>2;
+
+			uiData = imgIn->byImgR[3*(((2*r+0)*iWidthOriginal)+(2*c+0))+2]
+			+imgIn->byImgR[3*(((2*r+0)*iWidthOriginal)+(2*c+1))+2]
+			+imgIn->byImgR[3*(((2*r+1)*iWidthOriginal)+(2*c+0))+2]
+			+imgIn->byImgR[3*(((2*r+1)*iWidthOriginal)+(2*c+1))+2];
+			imgOut->byImgR[3*(r*iWidthNew+c)+2]=uiData>>2;
+		}}
+	}
+	return TRUE;
+}
+
+
 BOOL FindModel(ImgRGB* imgTarget, ImgRGB* imgModel, int iR0, int iC0, int iR1, int iC1, int* iFoundR, int* iFoundC)
 {
 	int iModelHeight;
@@ -430,6 +611,142 @@ BOOL FindModel(ImgRGB* imgTarget, ImgRGB* imgModel, int iR0, int iC0, int iR1, i
 		}
 		(*iFoundR)=iMaxR+iR0;
 		(*iFoundC)=iMaxC+iC0;
+		return TRUE;
+	}
+	return FALSE;
+}
+BOOL FindModelPylamid(ImgRGB* imgTarget, ImgRGB* imgModel, int iR0, int iC0, int iR1, int iC1, double dThreshPercent, int* iFoundR0, int* iFoundC0, int* iFoundR1, int* iFoundC1)
+{
+
+	if(imgTarget == NULL){return FALSE;}
+	if(imgModel == NULL){return FALSE;}
+	if(dThreshPercent<0){return FALSE;}
+	if(dThreshPercent>1){dThreshPercent=1;}
+	ImgRGB imgTargetTemp;
+	ImgRGB imgModelTemp;
+
+	CropImage2(imgTarget, &imgTargetTemp, iR0, iC0, iR1, iC1);
+	CropImage2(imgModel, &imgModelTemp, 0, 0, imgModel->iHeight-1, imgModel->iWidth-1);
+
+	
+	int iModelHeight;
+	int iModelWidth;
+	iModelHeight = imgModelTemp.iHeight;
+	iModelWidth = imgModelTemp.iWidth;
+
+	UINT uiThresh;
+
+	uiThresh=UINT(dThreshPercent*255*iModelHeight*iModelWidth);
+	UINT* uiMap;
+	int iMapW;
+	int iMapH;
+	iMapW=imgTargetTemp.iWidth-imgModelTemp.iWidth+1;
+	iMapH=imgTargetTemp.iHeight-imgModelTemp.iHeight+1;
+	if(iMapW<=0){return FALSE;}
+	if(iMapH<=0){return FALSE;}
+	uiMap = new UINT[iMapW*iMapH];
+	int iR1Local;
+	int iC1Local;
+
+	iR1Local = imgTargetTemp.iHeight-1;
+	iC1Local = imgTargetTemp.iWidth-1;
+
+
+	int iScanHeight;
+	int iScanWidth;
+	iScanHeight = iR1Local-iModelHeight + 2;
+	iScanWidth = iC1Local-iModelWidth + 2;
+
+	if(iScanHeight<=0){return FALSE;}
+	if(iScanWidth<=0){return FALSE;}
+	
+	BOOL bFound;
+
+
+	int iPtrTarget;
+	int iPtrModel;
+
+	memset(uiMap,0,iMapW*iMapH);
+	if((imgTarget->iChannel==CHANNEL_1_24) && (imgModel->iChannel == CHANNEL_3_8))
+	{
+		for(int iMapR=0; iMapR<iMapH; iMapR++)
+		{
+			for(int iMapC=0; iMapC<iMapW; iMapC++)
+			{
+
+				int iTargetR=iMapR;
+				int iTargetC=iMapC;
+				for(int r=0; r<iModelHeight; r++)
+				{
+					for(int c=0; c<iModelWidth; c++)
+					{
+						if(iTargetR + r>=imgTargetTemp.iHeight){uiMap[iMapR*iMapW+iMapC]+=255*3;break;}
+						if(iTargetC + c>=imgTargetTemp.iWidth){uiMap[iMapR*iMapW+iMapC]+=255*3;break;}
+						iPtrTarget = 3*((iTargetR + r)*imgTargetTemp.iWidth+(iTargetC+c));
+						iPtrModel = (r)*imgModelTemp.iWidth+(c);
+
+						uiMap[iMapR*iMapW+iMapC]+=bySubAbs(imgTargetTemp.byImgR[iPtrTarget + 0] , (imgModelTemp.byImgB[iPtrModel]));
+						uiMap[iMapR*iMapW+iMapC]+=bySubAbs(imgTargetTemp.byImgR[iPtrTarget + 2] , (imgModelTemp.byImgG[iPtrModel]));
+						uiMap[iMapR*iMapW+iMapC]+=bySubAbs(imgTargetTemp.byImgR[iPtrTarget + 3] , (imgModelTemp.byImgR[iPtrModel]));
+
+					}
+				}
+			}
+		}
+		int iScoreMax;
+		iScoreMax=0;
+		int iMaxR;
+		int iMaxC;
+
+
+		iMaxR=-1;
+		iMaxC=-1;
+		int iFoundR0Temp;
+		int iFoundR1Temp;
+		int iFoundC0Temp;
+		int iFoundC1Temp;
+		iFoundR0Temp=0;
+		for(int iMapR=0; iMapR<iMapH; iMapR++)
+		{
+			for(int iMapC=0; iMapC<iMapW; iMapC++)
+			{
+				if(uiMap[iMapR*iMapW+iMapC]>uiThresh){iFoundR0Temp=iMapR; break;}
+			}
+			if(iFoundR0Temp>0){break;}
+		}
+		iFoundR1Temp=iMapH-1;
+		for(int iMapR=iMapH-1; iMapR>=0; iMapR--)
+		{
+			for(int iMapC=0; iMapC<iMapW; iMapC++)
+			{
+				if(uiMap[iMapR*iMapW+iMapC]>uiThresh){iFoundR1Temp=iMapR; break;}
+			}
+			if(iFoundR1Temp<iMapH-1){break;}
+		}
+		iFoundC0Temp=0;
+		for(int iMapC=0; iMapC<iMapW; iMapC++)
+		{
+			for(int iMapR=0; iMapR<iMapH; iMapR++)
+			{
+				if(uiMap[iMapR*iMapW+iMapC]>uiThresh){iFoundC0Temp=iMapC; break;}
+			}
+			if(iFoundC0Temp>0){break;}
+		}
+
+		iFoundC1Temp=iMapW-1;
+		for(int iMapC=0; iMapC<iMapW; iMapC++)
+		{
+			for(int iMapR=0; iMapR<iMapH; iMapR++)
+			{
+				if(uiMap[iMapR*iMapW+iMapC]>uiThresh){iFoundC1Temp=iMapC; break;}
+			}
+			if(iFoundC1Temp<iMapW-1){break;}
+		}
+
+		*iFoundR0=iFoundR0Temp*2;
+		*iFoundR1=iFoundR1Temp*2+imgModel->iHeight;
+		*iFoundC0=iFoundC0Temp*2;
+		*iFoundC1=iFoundC1Temp*2+imgModel->iWidth;
 		return TRUE;
 	}
 	return FALSE;
