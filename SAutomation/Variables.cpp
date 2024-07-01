39758d48e12f254179258b37be418972061d0f39
Shogo Komatsu <49989073+ShougoKomatsu@users.noreply.github.com>
2024-07-01 13:00:17 +0900

    変数および変数ポインタへのアクセス関数を作成した

--- SAutomation/Variables.cpp
+++ SAutomation/Variables.cpp
@@ -4,3 +4,32 @@
 
 int g_iVar[MAX_THREAD][MAX_VARIABLES];
 
+int GetIntValue(int iScene, CString sArg)
+{
+	if(sArg.Left(6).CompareNoCase(_T("VarInt"))==0)
+	{
+		for(int iVarNameB1=1; iVarNameB1<=MAX_VARIABLES; iVarNameB1++)
+		{
+			CString sVarName;
+			sVarName.Format(_T("VarInt%d"));
+			if(sArg.CompareNoCase(sVarName)==0){return g_iVar[iScene][iVarNameB1-1];}
+		}
+	}
+	
+	return _ttoi(sArg);
+}
+
+int* GetIntValuePointer(int iScene, CString sArg)
+{
+	if(sArg.Left(6).CompareNoCase(_T("VarInt"))==0)
+	{
+		for(int iVarNameB1=1; iVarNameB1<=MAX_VARIABLES; iVarNameB1++)
+		{
+			CString sVarName;
+			sVarName.Format(_T("VarInt%d"));
+			if(sArg.CompareNoCase(sVarName)==0){return &(g_iVar[iScene][iVarNameB1-1]);}
+		}
+	}
+	
+	return NULL;
+}
--- SAutomation/Variables.h
+++ SAutomation/Variables.h
@@ -4,3 +4,5 @@
 #pragma once
 #define MAX_VARIABLES (8)
 extern int g_iVar[MAX_THREAD][MAX_VARIABLES];
+int GetIntValue(int iScene, CString sArg);
+int* GetIntValuePointer(int iScene, CString sArg);
