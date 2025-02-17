f3b3c7ed9cecc52f5ef75e6fe7b733edfbe5bef4
Shogo Komatsu <49989073+ShougoKomatsu@users.noreply.github.com>
2025-02-17 22:18:15 +0900

    10秒に１回、WindowHookをやり直すようにした

--- SAutomation/SAutomationDlg.cpp
+++ SAutomation/SAutomationDlg.cpp
@@ -21,6 +21,7 @@
 #define TIMER_DISP_MOUSPOS (100)
 #define TIMER_THREAD_WATCH (101)
 #define TIMER_WAKE_UP (102)
+#define TIMER_REHOOK (103)
 
 CStdioFile g_cf[MAX_THREAD];
 CString g_sLogFilePath[MAX_THREAD];
@@ -595,6 +596,7 @@ BOOL CSAutomationDlg::OnInitDialog()
 	SetTimer(TIMER_DISP_MOUSPOS,200, NULL);
 	SetTimer(TIMER_THREAD_WATCH,200, NULL);
 	SetTimer(TIMER_WAKE_UP, 100, NULL);
+	SetTimer(TIMER_REHOOK, 10000, NULL);
 
 	TCHAR szData[MAX_PATH];
 	GetCurrentDirectory(sizeof(szData)/sizeof(TCHAR),szData);
@@ -611,25 +613,25 @@ BOOL CSAutomationDlg::OnInitDialog()
 	CString sModelFolderPath;
 	sModelFolderPath.Format(_T("%s\\Macro\\Model"),m_sDir);
 
-	
-		m_tab.SubclassDlgItem(IDC_TAB_OPERATION, this);
 
-		m_tab.InsertItem(0, _T("0 - 15"));
-		m_tab.InsertItem(1, _T("16 - 31"));
-		m_tab.InsertItem(3, _T("32 - 47"));
-		m_tab.InsertItem(3, _T("48 - 63"));
-		
-		 
-		m_tabItem.Create(IDD_DIALOG_TAB_ITEMS, &m_tab);
+	m_tab.SubclassDlgItem(IDC_TAB_OPERATION, this);
+
+	m_tab.InsertItem(0, _T("0 - 15"));
+	m_tab.InsertItem(1, _T("16 - 31"));
+	m_tab.InsertItem(3, _T("32 - 47"));
+	m_tab.InsertItem(3, _T("48 - 63"));
+
+
+	m_tabItem.Create(IDD_DIALOG_TAB_ITEMS, &m_tab);
 	m_tabItem.pParent=this;	
-		CRect rect;
-		m_tab.GetWindowRect(rect);
-		m_tab.AdjustRect(FALSE, rect);
-		m_tab.ScreenToClient(rect);
-	
-		m_tabItem.MoveWindow(rect);
+	CRect rect;
+	m_tab.GetWindowRect(rect);
+	m_tab.AdjustRect(FALSE, rect);
+	m_tab.ScreenToClient(rect);
 
-		m_tabItem.ShowWindow(SW_SHOW);
+	m_tabItem.MoveWindow(rect);
+
+	m_tabItem.ShowWindow(SW_SHOW);
 
 	CFileFind cf;
 	if(cf.FindFile(sMacroFolderPath) != TRUE){_tmkdir(sMacroFolderPath);}
@@ -934,7 +936,12 @@ void CSAutomationDlg::RefreshTargetWindowPos()
 
 void CSAutomationDlg::OnTimer(UINT_PTR nIDEvent)
 {
-
+	
+	if(nIDEvent == TIMER_REHOOK)
+	{
+		ReHookWindowsHook();
+		return;
+	}
 	if(nIDEvent == TIMER_DISP_MOUSPOS)
 	{
 		UpdateData(TRUE);
