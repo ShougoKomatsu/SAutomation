242c109fc19fdc520cd976dda5114d65d44ccae5
Shogo Komatsu <49989073+ShougoKomatsu@users.noreply.github.com>
2025-04-24 13:05:53 +0900

    マクロ選択ダイアログのメンバにキーとファイル名を追加した

--- SAutomation/SelectDlg.h
+++ SAutomation/SelectDlg.h
@@ -8,6 +8,9 @@ class CSelectDlg : public CDialogEx
 	DECLARE_DYNAMIC(CSelectDlg)
 
 public:
+	CString m_sResultFileName;
+	CString m_sSelectKeys[8];
+	CString m_sSelectFiless[8];
 	CSelectDlg(CWnd* pParent = NULL);   // 標準コンストラクター
 	virtual ~CSelectDlg();
 
