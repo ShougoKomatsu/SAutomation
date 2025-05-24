d82ebab3d1a56a11e1d8c9d57d5290ba209a576c
Shogo Komatsu <49989073+ShougoKomatsu@users.noreply.github.com>
2025-05-24 11:10:15 +0900

    不要なマクロ宣言を削除した

--- SAutomation/Variables_Int.h
+++ SAutomation/Variables_Int.h
@@ -5,26 +5,26 @@
 
 
 BOOL GetOperandIntSrc(CString sDataLine, int* iCommandType);
+BOOL GetOperandPointSrc(CString sDataLine, int* iCommandType);
+BOOL GetOperandRectSrc(CString sDataLine, int* iCommandType);
 
 
 int GetIntValue(CString sDir, int iScene, CString sDataLocal);
+BOOL GetRectValue(CString sDir, int iScene, CString sArg, CRect* rectOutput);
+BOOL GetPointValue(CString sDir, int iScene, CString sDataLocal, Point* pPoint);
+
 int* GetIntValuePointer(CString sDir, int iScene, CString sArg);
+Point* GetPointValuePointer(CString sDir, int iScene, CString sArg);
+CRect* GetRectValuePointer(CString sDir, int iScene, CString sArg);
+
+
+
+ReturnValue SetIntValue(CString sDir, int iScene, int* iDstPointer, CString sDataLocal, int iSelfSrc);
+ReturnValue SetPointValue(CString sDir, int iScene, Point* pPoint, CString sDataLocal);
+ReturnValue SetRectValue(CString sDir, int iScene, CRect* pRect, CString sDataLocal);
+
+
 int IntAdd(CString sDir, int iScene, CString sArg1, CString sArg2);
 int IntSub(CString sDir, int iScene, CString sArg1, CString sArg2);
 int IntMult(CString sDir, int iScene, CString sArg1, CString sArg2);
 int IntDiv(CString sDir, int iScene, CString sArg1, CString sArg2);
-
-
-ReturnValue SetIntValue(CString sDir, int iScene, int* iDstPointer, CString sDataLocal, int iSelfSrc);
-
-BOOL GetOperandPointSrc(CString sDataLine, int* iCommandType);
-
-Point* GetPointValuePointer(CString sDir, int iScene, CString sArg);
-
-ReturnValue SetPointValue(CString sDir, int iScene, Point* pPoint, CString sDataLocal);
-CRect* GetRectValuePointer(CString sDir, int iScene, CString sArg);
-ReturnValue SetRectValue(CString sDir, int iScene, CRect* pRect, CString sDataLocal);
-BOOL GetOperandRectSrc(CString sDataLine, int* iCommandType);
-
-BOOL GetRectValue(CString sDir, int iScene, CString sArg, CRect* rectOutput);
-BOOL GetPointValue(CString sDir, int iScene, CString sDataLocal, Point* pPoint);
--- SAutomation/Variables_String.h
+++ SAutomation/Variables_String.h
@@ -3,34 +3,6 @@
 #include "stdafx.h"
 #include "common.h"
 
-
-#define FORMATO (_T("%d"))
-#define FORMAT_FLAG_MASK			(0x0000F000)
-#define FORMAT_FLAG_NOTHING			(0x00000000)
-#define FORMAT_FLAG_SPACE			(0x00001000)
-#define FORMAT_FLAG_PLUS			(0x00002000)
-#define FORMAT_FLAG_ZERO			(0x00003000)
-
-#define FORMAT_MIN_FIELD_MASK		(0x000F0000)
-#define FORMAT_MIN_FIELD_SHIFT		(16)
-
-#define FORMAT_ACCURACY_MASK		(0x00F00000)
-#define FORMAT_ACCURACY_SHIFT		(20)
-
-
-#define FORMAT_QUALIFIER_MASK		(0x03000000)
-#define FORMAT_QUALIFIER_NOTHING	(0x00000000)
-#define FORMAT_QUALIFIER_HALF		(0x01000000)
-#define FORMAT_QUALIFIER_LONG		(0x02000000)
-
-
-#define FORMAT_SPECIFIER_MASK		 (0xF0000000)
-#define FORMAT_SPECIFIER_DECIMAL	 (0x10000000)
-#define FORMAT_SPECIFIER_SMALL_HEX	 (0x20000000)
-#define FORMAT_SPECIFIER_CAPITAL_HEX (0x30000000)
-#define FORMAT_SPECIFIER_UNSIGNED	 (0x40000000)
-
-
 BOOL GetOperandStrSrc(CString sDataLine, int* iCommandType);
 const CString GetStrValue(CString sDir, int iScene, CString sDataLocal);
 CString* GetStrValuePointer(CString sDir, int iScene, CString sArg);
